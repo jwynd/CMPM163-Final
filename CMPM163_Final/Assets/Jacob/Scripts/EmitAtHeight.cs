f5b75efb7c781c76b25f2b4e310e5eddb1415f1b
jwynd <18224844+jwynd@users.noreply.github.com>
2019-06-03 21:00:11 -0700

    Particle system introduction, tweaks to come

---
+++ CMPM163_Final/Assets/Jacob/Scripts/EmitAtHeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(ParticleSystem))]
+public class EmitAtHeight : MonoBehaviour
+{
+    [SerializeField][Range(0.01f,1.0f)]private float sensitivity = 0.5f;
+    private ParticleSystem ps;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = this.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.gameObject.name + " Should be emitting "+(int)Mathf.Floor(this.transform.localPosition.y * 5) +" particles");
+        ps.Emit((int)Mathf.Floor(this.transform.localPosition.y * 5));
+    }
+}
